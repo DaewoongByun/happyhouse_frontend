f75218d600e9e100fb8df84d28b386be1ac79a5a
seo min woo <smw123123@naver.com>
2021-05-25 17:43:46 +0900

    이미지

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,26 @@
     <div class="home__searchbar">
       <search-bar></search-bar>
     </div>
-    <div>
-      <img id="backimg" src="../assets/house/wallpaper6.jpg" />
+    <div id="backimg">
+      <!-- <img id="backimg" src="../assets/house/wallpaper6.jpg" /> -->
     </div>
   </div>
 </template>
 
 <script>
+  let nystories = document.querySelector("div").offsetTop;
+  window.onscroll = function () {
+    if (window.pageYOffset > 0) {
+      let opac = window.pageYOffset / nystories;
+      console.log(opac);
+      document.getElementById("backimg").style.background =
+        "linear-gradient(rgba(255, 255, 255, " +
+        opac +
+        "), rgba(255, 255, 255, " +
+        opac +
+        ")), url(../assets/house/wallpaper6.jpg)";
+    }
+  };
   import SearchBar from "../components/home/SearchBar";
   export default {
     name: "Home",
@@ -21,7 +34,7 @@
 
 <style scoped>
   #home {
-    min-height: 600px;
+    min-height: 1000px;
   }
   .home__searchbar {
     height: 50px;
@@ -33,5 +46,18 @@
   img:hover {
     filter: brightness(60%);
     transition: filter 1s;
+  }
+
+  #backimg {
+    height: 700px;
+    width: 100%;
+    background-image: url("../assets/house/wallpaper6.jpg");
+    background-repeat: no-repeat;
+    background-attachment: fixed !important;
+    background-size: 100% !important;
+    background-position: center top !important;
+    padding: 1rem;
+    padding-top: 45%;
+    color: #fff;
   }
 </style>
